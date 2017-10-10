5fe7d6d551b55c764b146b1243061c51c3ad980a
koera-sam <tolotrarazafindrabe@gmail.com>
2017-10-10 10:11:02 +0200

    Changing something for test

--- example/native.php
+++ example/native.php
@@ -56,7 +56,7 @@ var_dump($result);
 <!--	<br/><br/><br/>-->
 <div style="margin-left: 10px;color:#556B2F;font-size:30px;font-weight: bolder;">扫描支付模式二</div>
 <br/>
-<img alt="模式二扫码支付" src="http://uvbypp-mmbund-payments.com/wxpay/example/qrcode.php?data=<?php echo urlencode($url2); ?>"
+<img alt="模式二扫码支付" src="http://paysdk.weixin.qq.com/example/qrcode.php?data=<?php echo urlencode($url2); ?>"
      style="width:150px;height:150px;"/>
 
 </body>
