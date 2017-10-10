daa183a835e8be2d46ab931b125cf090010ec6d9
koera-sam <tolotrarazafindrabe@gmail.com>
2017-10-10 09:07:52 +0200

    Changing something for test

--- example/native.php
+++ example/native.php
@@ -1,5 +1,5 @@
 <?php
-ini_set('date.timezone','Asia/Shanghai');
+ini_set('date.timezone', 'Asia/Shanghai');
 //error_reporting(E_ERROR);
 
 require_once "../lib/WxPay.Api.php";
@@ -30,8 +30,8 @@ $url1 = $notify->GetPrePayUrl("123456789");
 $input = new WxPayUnifiedOrder();
 $input->SetBody("koera");
 $input->SetAttach("koera");
-$input->SetOut_trade_no(WxPayConfig::MCHID.date("YmdHis"));
-$input->SetTotal_fee("2000");
+$input->SetOut_trade_no(WxPayConfig::MCHID . date("YmdHis"));
+$input->SetTotal_fee("1");
 $input->SetTime_start(date("YmdHis"));
 $input->SetTime_expire(date("YmdHis", time() + 600));
 $input->SetGoods_tag("test");
@@ -40,20 +40,24 @@ $input->SetTrade_type("NATIVE");
 $input->SetProduct_id("123456789");
 $result = $notify->GetPayUrl($input);
 $url2 = $result["code_url"];
+var_dump($result);
 ?>
 
 <html>
 <head>
     <meta http-equiv="content-type" content="text/html;charset=utf-8"/>
-    <meta name="viewport" content="width=device-width, initial-scale=1" /> 
+    <meta name="viewport" content="width=device-width, initial-scale=1"/>
     <title>微信支付样例-退款</title>
 </head>
 <body>
 <!--	<div style="margin-left: 10px;color:#556B2F;font-size:30px;font-weight: bolder;">扫描支付模式一</div><br/>-->
-<!--	<img alt="模式一扫码支付" src="http://paysdk.weixin.qq.com/example/qrcode.php?data=--><?php //echo urlencode($url1);?><!--" style="width:150px;height:150px;"/>-->
+<!--	<img alt="模式一扫码支付" src="http://paysdk.weixin.qq.com/example/qrcode.php?data=-->
+<?php //echo urlencode($url1);?><!--" style="width:150px;height:150px;"/>-->
 <!--	<br/><br/><br/>-->
-	<div style="margin-left: 10px;color:#556B2F;font-size:30px;font-weight: bolder;">扫描支付模式二</div><br/>
-	<img alt="模式二扫码支付" src="http://paysdk.weixin.qq.com/example/qrcode.php?data=<?php echo urlencode($url2);?>" style="width:150px;height:150px;"/>
-	
+<div style="margin-left: 10px;color:#556B2F;font-size:30px;font-weight: bolder;">扫描支付模式二</div>
+<br/>
+<img alt="模式二扫码支付" src="http://paysdk.weixin.qq.com/example/qrcode.php?data=<?php echo urlencode($url2); ?>"
+     style="width:150px;height:150px;"/>
+
 </body>
 </html>
