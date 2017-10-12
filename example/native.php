6538d84565523d043342d512caafca7ac09352ba
koera-sam <tolotrarazafindrabe@gmail.com>
2017-10-12 10:29:41 +0200

    _

--- example/native.php
+++ example/native.php
@@ -50,6 +50,14 @@ $url2 = $result["code_url"];
     <title>微信支付样例-退款</title>
 </head>
 <body>
+<?php
+if (isset($_GET['id'])) {
+    $booking_id = $_GET['id'];
+    echo $booking_id;
+}
+
+?>
+
 <!--	<div style="margin-left: 10px;color:#556B2F;font-size:30px;font-weight: bolder;">扫描支付模式一</div><br/>-->
 <!--	<img alt="模式一扫码支付" src="http://paysdk.weixin.qq.com/example/qrcode.php?data=-->
 <?php //echo urlencode($url1);?><!--" style="width:150px;height:150px;"/>-->
