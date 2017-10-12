f213caa563d5cbd69b4e7bfc91e91ba79d70d412
koera-sam <tolotrarazafindrabe@gmail.com>
2017-10-12 10:33:41 +0200

    _

--- lib/WxPay.Api.php
+++ lib/WxPay.Api.php
@@ -57,7 +57,7 @@ class WxPayApi
         //签名
         $inputObj->SetSign();
         $xml = $inputObj->ToXml();
-        var_dump($xml);
+//        var_dump($xml);
         $startTimeStamp = self::getMillisecond();//请求开始时间
         $response = self::postXmlCurl($xml, $url, false, $timeOut);
 //        var_dump($response);
